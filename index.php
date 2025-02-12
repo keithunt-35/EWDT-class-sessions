fe0736d8e01e82037aaace3ec8e8a3509b9004a5
keithunt-35 <keithunt35@duck.com>
2025-02-12 15:20:01 +0300

    skipping through loops and switches

--- index.php
+++ index.php
@@ -14,9 +14,41 @@
     print(gettype($num));
 
     $num1= array (10.5);
-    print(gettype($num1))
+    print(gettype($num1));
 
-    
+    // white space insensitive
+    $x  =    1  +  10;
+    $y =4-3;
+    print($x);
+    echo $y;
+
+
+    //constants
+    define("MINSIZE", 50);
+    echo MINSIZE; 
+
+    print("<br>");
+    print(__LINE__);//prints the line at which you are
+    print("<br>");
+    print(__FILE__);// prints the file name
+    print("<br>");
+    print(__DIR__);// prints the directory name
+    print("<br>");
+
+    $marks = 10+20;
+    print($marks);
+
+    //the differnce between echo and print is that echo can take multiple parameters but print can only take one parameter
+    echo "hello", "world";
+    print("hello");
+    print("world");
+    print("<br>");
+    $marks1 = array(10,20,30,60);
+    foreach($marks1 as $i){
+        print($i." ,");
+        print("<br>");
+    }
+
 
 
     ?>
