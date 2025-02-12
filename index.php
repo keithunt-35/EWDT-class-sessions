738a9ee2a35245d0c5bb87f05985a8b4a72e3724
keithunt-35 <keithunt35@duck.com>
2025-02-12 14:55:21 +0300

    this is the first session

---
+++ index.php
@@ -0,0 +1,22 @@
+
+    
+    changed
+    <?php
+    echo "hello world";
+    print ("hello students");
+    #this is a comment
+    //this is also a comment
+    /*
+    noe this is a multi line comment
+    */
+    
+    $num =10;
+    print(gettype($num));
+
+    $num1= array (10.5);
+    print(gettype($num1))
+
+    
+
+
+    ?>
---
+++ lesson1.php
@@ -0,0 +1,3 @@
+<html>
+    less
+</html>
